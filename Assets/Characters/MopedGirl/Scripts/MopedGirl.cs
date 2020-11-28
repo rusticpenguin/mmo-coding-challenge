830320c02303b17ad778bebf4e6acc123f0f548d
DESKTOP-L3453CH\Finalsmoothie <hello@georgechios.com>
2020-11-28 15:36:05 -0700

    Creates code challenge

---
+++ Assets/Characters/MopedGirl/Scripts/MopedGirl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MopedGirl : MonoBehaviour
+{
+    [SerializeField] int health = 12;
+    [SerializeField] int exp = 0;
+
+    float accel = 0f;
+    [SerializeField] float currentYSpeed = 0f;
+    [SerializeField] float maxYSpeed = 4f;
+    [SerializeField] float xSpeed = 2f;
+
+    void Update()
+    {
+        VerticalMovement();
+    }
+
+    void VerticalMovement ()
+    {
+        if (currentYSpeed < maxYSpeed)
+        {
+            currentYSpeed += accel;
+        }
+
+        transform.Translate(Vector3.up * currentYSpeed * Time.deltaTime);
+    }
+
+    void IncreaseExp(int experience)
+    {
+        exp += experience;
+    }
+}
